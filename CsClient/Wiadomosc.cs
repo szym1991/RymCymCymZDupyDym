dd447f2a5b39beed57347524b2bb71b0cbe6a4e6
unknown <Blazej@.(none)>
2012-01-20 13:58:39 +0100

    Opracowanie AutoSpeaka, kurde!

---
+++ CsClient/Wiadomosc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CsClient
+{
+    class Wiadomosc
+    {
+        public String author;
+        public String komunikat;
+        public bool odpowiedzialem;
+
+       public Wiadomosc(String a, String b)
+        {
+            this.author = a;
+            this.komunikat = b;
+            this.odpowiedzialem = false;
+           
+        }
+    }
+}
